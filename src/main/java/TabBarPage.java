14d18cafd248af1fad96741ea82fc72260119df5
Furkan Yıldırım <yildirim.frkn@outlook.com>
2024-09-02 11:41:45 +0300

    page locators

--- src/main/java/TabBarPage.java
+++ src/main/java/TabBarPage.java
@@ -1,2 +1,25 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+
 public class TabBarPage {
+    WebDriver driver;
+
+    By searchLocator = By.id("searchData");
+
+    By totalNumLocator = By.className("basketTotalNum ");
+
+
+    public TabBarPage(WebDriver driver){
+        this.driver = driver;
+    }
+
+    public void search(String value){
+        driver.findElement(searchLocator).sendKeys(value+ Keys.ENTER);
+    }
+
+    public String getTotalNum(){
+        return driver.findElement(totalNumLocator).getText();
+    }
+
 }
---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,32 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.asserts.SoftAssert;
+
+import java.time.Duration;
+
+public class BaseTest {
+
+    WebDriver driver;
+
+    SoftAssert softAssert;
+
+    @BeforeClass
+    public void setUp(){
+        WebDriverManager.chromiumdriver().setup();
+        driver = new ChromeDriver();
+        driver.get("https://www.n11.com/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        softAssert = new SoftAssert();
+    }
+
+
+    @AfterClass
+    public void tearDown(){
+        driver.quit();
+        softAssert.assertAll();
+    }
+}
